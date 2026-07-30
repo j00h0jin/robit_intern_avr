45a41f1f58b8e04f918801930aa5bf04033ef753
j00h0jin <hojin7710@daum.net>
2026-07-30 18:17:56 +0900

    ✅ day02 task02

--- day2/Day02_Task02/LCD_Text.h
+++ day2/Day02_Task02/LCD_Text.h
@@ -1,68 +1,37 @@
-﻿/*==============================================================================
- *
- *   CLCD
- *		LCD Driver		: KS0066U
- *
- *   	File Name   	: LCD_Text.h
- *  	Version        	: 2.0
- *    	Date           	: Dec 24, 2013
- *		Modified       	: Dec 9, 2016 By Kim Jihun
- *		Author         	: KimJihun  (ROBIT 9th, Kwangwoon University)
- *		MPU_Type       	: Atmega128 (16MHz)
- *		Compiler		: AtmelStudio6 6.1
- *		Copyright(c) 2016 ROBIT, Kwangwoon University.
- *    	All Rights Reserved.
- *
-==============================================================================*/
-
-#define F_CPU 16000000
+﻿#ifndef LCD_TEXT_H_
+#define LCD_TEXT_H_
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdio.h>
-
-#ifndef __Text_Lcd_H
-#define __Text_Lcd_H
+#include <stdint.h>
 
 
+//==============================
+// Software I2C Pin Definition
+//==============================
+#define I2C_DDR     DDRD
+#define I2C_PORT    PORTD
+#define I2C_PIN     PIND
 
-//------------------------------------------------------------------------------
-//                      Control, Data PORTX
-//	Bit			7		6		5		4		3		2		1		0
-//	LCD Pin		DB7		DB6		DB5		DB4		X		E		R/W		RS
-//------------------------------------------------------------------------------
-#define LCD_PORT			PORTC
-#define LCD_PORT_SETUP()	DDRC = 0xFF
-
-//Text Lcd 명령어 셋팅
-#define FUNCSET		0x28	//Function set		0010 1000
-#define ENTMODE		0x06	//Entry Mode Set	0000 0110
-#define ALLCLR		0x01	//All Clear			0000 0001
-#define DISPON		0x0c	//Display On		0000 1100
-#define LINE1		0x80	//1st line Move		1000 0000
-#define LINE2		0xc0	//2nd line Move		1100 0000
-
-#define U8 unsigned char
-#define U16 unsigned int
+#define SDA         PD1
+#define SCL         PD0
 
 
+//==============================
+// PCF8574T Address
+//==============================
+#define LCD_ADDR    0x27
 
-//------------------------------------------------------------------------------
-//                       ===== Funtion Prototype =====
-//------------------------------------------------------------------------------
+
+//==============================
+// LCD Function
+//==============================
 void lcdInit(void);
 void lcdClear(void);
-void lcdString(U8 line, U8 col, char  *str);
-void lcdNumber(U8 line, U8 col, int num);
 
+void lcdString(uint8_t row, uint8_t col, char *str);
+void lcdNumber(uint8_t row, uint8_t col, int number);
+void lcdFloat(uint8_t row,uint8_t col,float number, uint8_t decimals);
 
-
-
-void lcdCommand(U8 byte);
-void lcdData(U8 byte);
-void lcdDisplayPosition( U8 line, U8 col );
-
-void Busy(void);
-void delay_us(U16 time_us);
 
 #endif
--- day2/Day02_Task02/main.c
+++ day2/Day02_Task02/main.c
@@ -4,15 +4,36 @@
  * Created: 2026-07-29 오전 10:47:33
  * Author : hojin
  */ 
-
+#define F_CPU 16000000
+#include "LCD_Text.h"
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int Read_ADC() {
+	ADCSRA |= (1 << ADSC); // 변환 시작 ADSC = 1
+	return ADC;
+}
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	int r;
+	float v;
+	ADMUX = 0x40; // 0100 0000 외부의 AVCC 전압 사용
+	ADCSRA = 0x87; // 1000 0111 ADC 활성화, 클럭 분주비 128
+
+	lcdInit();
+	lcdClear();
+
+	while (1)
+	{
+		r=Read_ADC();
+		v=(float)r*5/1024;
+		lcdString(0,0,"Jhj"); // row col txt
+		lcdNumber(1,0,r); // row col num
+		lcdFloat(1,6,v,1); // row col num decimal
+		lcdString(1,9,"V");
+	}
 }
 
