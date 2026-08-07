f02ca87a77cb2886e4431e6762d5b9b9064605a7
j00h0jin <hojin7710@daum.net>
2026-08-08 03:10:25 +0900

    🌱 add task project

---
+++ src/day6/Day06_Task01/main.c
@@ -0,0 +1,18 @@
+/*
+ * Day06_Task01.c
+ *
+ * Created: 2026-08-08 오전 3:06:37
+ * Author : hojin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ src/day6/Day06_Task02/main.c
@@ -0,0 +1,18 @@
+/*
+ * Day06_Task02.c
+ *
+ * Created: 2026-08-08 오전 3:07:30
+ * Author : hojin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
